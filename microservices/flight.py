ee6f225f11b4f4232e13c2c3ef9a35ab31222341
Ngee Tian <warkionw12@gmail.com>
2024-03-30 13:35:15 +0800

    Added flights to yml, added get all, search, fixed errors

--- microservices/flight.py
+++ microservices/flight.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
 from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
@@ -13,18 +14,20 @@ db = SQLAlchemy(app)
 CORS(app)
 
 class Flight(db.Model):
-    __tablename__ = 'Flight'
+    __tablename__ = 'flights'
 
     FID = db.Column(db.String(6), primary_key=True)
+    Airline = db.Column(db.String(255))
     DepartureLoc = db.Column(db.String(255), nullable=False)
     ArrivalLoc = db.Column(db.String(255), nullable=False)
-    Date = db.Column(db.DateTime, nullable=False)
-    DepartureTime = db.Column(db.DateTime, nullable=False)
-    Duration = db.Column(db.Int, nullable=False)
+    Date = db.Column(db.Date, nullable=False)
+    DepartureTime = db.Column(db.Time, nullable=False)
+    Duration = db.Column(db.Integer, nullable=False)
     Price = db.Column(db.Float(precision=2), nullable=False)
 
-    def __init__(self, FID, DepartureLoc, ArrivalLoc, Date, DepartureTime, Duration, Price):
+    def __init__(self, FID, Airline, DepartureLoc, ArrivalLoc, Date, DepartureTime, Duration, Price):
         self.FID = FID
+        self.Airline = Airline
         self.DepartureLoc = DepartureLoc
         self.ArrivalLoc = ArrivalLoc
         self.Date = Date
@@ -33,26 +36,72 @@ class Flight(db.Model):
         self.Price = Price
 
     def json(self):
-        return {"FID": self.FID, "DepartureLoc": self.DepartureLoc, "ArrivalLoc": self.ArrivalLoc, "Date": self.Date,
-                "DepartureTime": self.DepartureTime, "Duration": self.Duration, "Price": self.Price}
+        return {
+                "FID": self.FID, 
+                "Airline": self.Airline,
+                "DepartureLoc": self.DepartureLoc, 
+                "ArrivalLoc": self.ArrivalLoc, 
+                "Date": self.Date,
+                "DepartureTime": str(self.DepartureTime), 
+                "Duration": self.Duration, 
+                "Price": self.Price
+                }
     
     def getPrice(self):
         return {"Price": self.Price}
-    
-# Search Flights, returns ALL flights if no user inputs
-@app.route("/flight")
-def find_by_FID(FID):
-    DepartureLoc = request.args.get('DepartureLoc', None)
-    ArrivalLoc = request.args.get('ArrivalLoc', None)
-    DepartureDate = request.args.get('DepartureDate', None)
 
-    flightList = db.session.scalars(db.select(Flight).filter_by(DepartureLoc=DepartureLoc, ArrivalLoc=ArrivalLoc, DepartureDate=DepartureDate)).all()
+
+@app.route("/flight")
+def get_all_flights():
+    flightlist = db.session.scalars(db.select(Flight)).all()
+    if len(flightlist):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "flights": [(flight.json()) for flight in flightlist]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no flights."
+        }
+    ), 404
+
+## JSON to pass in  ##
+# EXISTS
+# {
+#     "DepartureLoc": "Singapore",
+#     "ArrivalLoc": "Berlin",
+#     "DepartureDate": "2024-03-15"
+# }
+
+# DON'T EXISTS
+# {
+#     "DepartureLoc": "Singapore",
+#     "ArrivalLoc": "Taipei",
+#     "DepartureDate": "2024-03-15"
+# }
+
+
+@app.route("/flight",  methods=['POST'])
+def searchflights():
+    DepartureLoc = request.json.get('DepartureLoc', None)
+    ArrivalLoc = request.json.get('ArrivalLoc', None)
+    DepartureDate = request.json.get('DepartureDate', None)
+
+    # Convert DepartureDate to a datetime object
+    DepartureDate = datetime.strptime(DepartureDate, '%Y-%m-%d')
+
+    flightList = db.session.scalars(db.select(Flight).filter_by(DepartureLoc=DepartureLoc, ArrivalLoc=ArrivalLoc, Date=DepartureDate)).all()
     if len(flightList):
         return jsonify(
             {
                 "code": 200,
                 "data": {
-                    "flights": [flight.json() for flight in flightList]
+                    "flights": [(flight.json()) for flight in flightList]
                 }
             }
         )
