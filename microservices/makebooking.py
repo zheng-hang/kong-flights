fd5ac31599bad01263954597431427e44e5063d8
Ngee Tian <warkionw12@gmail.com>
2024-04-01 17:26:17 +0800

    makebooking - not yet tested - no error handling

--- microservices/makebooking.py
+++ microservices/makebooking.py
@@ -46,7 +46,7 @@ if not amqp_connection.check_exchange(channel, booking_exchangename, exchangetyp
 # JSON RESPONSE FORMAT
 # {
 #     'email': 'help@gmail.com',
-#     'FID': 'SQ 123',
+#     'fid': 'SQ 123',
 #     'seatcol': 'A',
 #     'seatnum': 1
 # }
@@ -116,6 +116,11 @@ def processBookingRequest(booking):
 
     else:
         print('\n-----'+ price['message'] +'-----')
+        return {
+            "code": 500,
+            "data": {"priceRetrieval_result": price},
+            "message": "Price not found."
+        }
 
 
     if payment_result["code"] in range (200,300):
@@ -141,93 +146,19 @@ def processBookingRequest(booking):
         print("\nBooking creation request published to the RabbitMQ Exchange:", booking)
 
 
+        return jsonify({
+                    'code': 200,
+                    'message': "Payment successful, booking creation sent"
+                })
 
-
-    # if code not in range(200, 300):
-    #     # Inform the error microservice
-    #     #print('\n\n-----Invoking error microservice as order fails-----')
-    #     print('\n\n-----Publishing the (order error) message with routing_key=order.error-----')
-
-    #     # invoke_http(error_URL, method="POST", json=order_result)
-    #     channel.basic_publish(exchange=exchangename, routing_key="order.error", 
-    #         body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
-    #     # make message persistent within the matching queues until it is received by some receiver 
-    #     # (the matching queues have to exist and be durable and bound to the exchange)
-
-    #     # - reply from the invocation is not used;
-    #     # continue even if this invocation fails        
-    #     print("\nOrder status ({:d}) published to the RabbitMQ Exchange:".format(
-    #         code), order_result)
-
-    #     # 7. Return error
-    #     return {
-    #         "code": 500,
-    #         "data": {"order_result": order_result},
-    #         "message": "Order creation failure sent for error handling."
-    #     }
-
-
-    # Notice that we are publishing to "Activity Log" only when there is no error in order creation.
-    # In http version, we first invoked "Activity Log" and then checked for error.
-    # Since the "Activity Log" binds to the queue using '#' => any routing_key would be matched 
-    # and a message sent to “Error” queue can be received by “Activity Log” too.
-
-    # else:
-    #     # 4. Record new order
-    #     # record the activity log anyway
-    #     #print('\n\n-----Invoking activity_log microservice-----')
-    #     print('\n\n-----Publishing the (order info) message with routing_key=order.info-----')        
-
-    #     # invoke_http(activity_log_URL, method="POST", json=order_result)            
-    #     channel.basic_publish(exchange=exchangename, routing_key="order.info", 
-    #         body=message)
-    
-    print("\nOrder published to RabbitMQ Exchange.\n")
-    # - reply from the invocation is not used;
-    # continue even if this invocation fails
-    
-    # 5. Send new order to shipping
-    # Invoke the shipping record microservice
-    print('\n\n-----Invoking shipping_record microservice-----')    
-    
-    shipping_result = invoke_http(
-        shipping_record_URL, method="POST", json=order_result['data'])
-    print("shipping_result:", shipping_result, '\n')
-
-    # Check the shipping result;
-    # if a failure, send it to the error microservice.
-    code = shipping_result["code"]
-    if code not in range(200, 300):
-        # Inform the error microservice
-        #print('\n\n-----Invoking error microservice as shipping fails-----')
-        print('\n\n-----Publishing the (shipping error) message with routing_key=shipping.error-----')
-
-        # invoke_http(error_URL, method="POST", json=shipping_result)
-        message = json.dumps(shipping_result)
-        channel.basic_publish(exchange=exchangename, routing_key="shipping.error", 
-            body=message, properties=pika.BasicProperties(delivery_mode = 2))
-
-        print("\nShipping status ({:d}) published to the RabbitMQ Exchange:".format(
-            code), shipping_result)
-
-        # 7. Return error
-        return {
+    return {
             "code": 400,
             "data": {
-                "order_result": order_result,
-                "shipping_result": shipping_result
+                "payment_result": payment_result,
             },
             "message": "Simulated shipping record error sent for error handling."
         }
 
-    # 7. Return created order, shipping record
-    return {
-        "code": 201,
-        "data": {
-            "order_result": order_result,
-            "shipping_result": shipping_result
-        }
-    }
 
 
 # Execute this program if it is run as a main script (not by 'import')
