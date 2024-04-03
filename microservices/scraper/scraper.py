807f9b636ab478f03420b129c1d3ee202a829d6b
Ngee Tian <warkionw12@gmail.com>
2024-04-03 17:10:22 +0800

    pain

--- microservices/scraper/scraper.py
+++ microservices/scraper/scraper.py
@@ -1,63 +1,39 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
-
-
+from os import environ
+from invokes import invoke_http
 import json
 
-import os, sys
-from os import environ
-
-from invokes import invoke_http
-
-
-
-# Init flask app
 app = Flask(__name__)
 CORS(app)
 
-
-# Declare ENV vars
-# luft_URL = "http://localhost:5004/getLHflights/today"
 ryanair_URL = environ.get('ryanair_URL')
 lufthansa_URL = environ.get('lufthansa_URL')
 flight_URL = environ.get('flight_URL')
 seat_URL = environ.get('seat_URL')
 
-# Get the JSON data from the API
-# response = requests.get(luft_URL)
-# data = response.json()
-
-# @app.route('/insertflights', methods=['POST'], json=data)
-
-# xinsert_flights_URL = "http://your-api-url/insertflights"
-
-
 @app.route("/scrapeAPIs")
 def scrapeAPI():
-    # Call lufthansa API
-    insert_LH_flight = invoke_http(lufthansa_URL, method='GET')
+    all_flights = []
 
+    # Call Lufthansa API
+    insert_LH_flight = invoke_http(lufthansa_URL, method='GET')
     if insert_LH_flight['code'] == 200:
         data = insert_LH_flight['data']
-        all_LHresponses = []
-
         for date, flights in data.items():
-            all_LHresponses.extend(flights)
-
-        print(all_LHresponses)
+            all_flights.extend(flights)
     else:
-        print("Failed to get responses. Response code:", insert_LH_flight['code'])
+        print("Failed to get responses from Lufthansa API. Response code:", insert_LH_flight['code'])
 
-
-    # Call ryanair API
-    insert_FR_flight = invoke_http(ryanair_URL, method='GET')['data']
-
-    print(insert_FR_flight)
+    # Call Ryanair API
+    insert_FR_flight = invoke_http(ryanair_URL, method='GET')
+    if insert_FR_flight['code'] == 200:
+        all_flights.extend(insert_FR_flight['data'])
+    else:
+        print("Failed to get responses from Ryanair API. Response code:", insert_FR_flight['code'])
 
     # Merge both lists
-    all_flights = {"flight": insert_FR_flight + all_LHresponses}
-
-    print(all_flights)
+    all_flights_data = {"flight": all_flights}
 
     # Read seat layout from seat_layout.json
     with open('seat_layout.json', 'r') as f:
@@ -65,34 +41,24 @@ def scrapeAPI():
 
     flight_seats = {}
 
-    for flight in all_flights['flight']:
+    for flight in all_flights_data['flight']:
         fid = flight['FID']
         flight_seats[fid] = [{'seatcol': seat['seatcol'], 'seatnum': seat['seatrow']} for seat in seat_layout]
 
-    # Insert into flights DB, returned json
-    flight_results = invoke_http(flight_URL, method='POST', json=all_flights)
+    # Insert into flights DB
+    flight_results = invoke_http(flight_URL, method='POST', json=all_flights_data)
 
-    # Insert into seats DB, returned json
+    # Insert into seats DB
     seat_results = invoke_http(seat_URL, method='POST', json=flight_seats)
 
     results = flight_results + seat_results
 
     return jsonify(results)
 
-
-@app.route("/scrapeAPIs")
-def manualcall():
-    results = scrapeAPI()
-    return results
-
-
-
-
-
-# Execute this program if it is run as a main script (not by 'import')
 if __name__ == "__main__":
-    print("This is flask " + os.path.basename(__file__) + " for placing an order...")
+    print("Starting the scraper service...")
     app.run(host="0.0.0.0", port=5100, debug=True)
+
 
 
 
